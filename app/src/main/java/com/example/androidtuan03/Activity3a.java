22b6ffce59cad596d4fdfb182efc00f6af75e408
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-02-28 17:29:08 +0700

    finsh

---
+++ app/src/main/java/com/example/androidtuan03/Activity3a.java
@@ -0,0 +1,14 @@
+package com.example.androidtuan03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity3a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity3a);
+    }
+}
