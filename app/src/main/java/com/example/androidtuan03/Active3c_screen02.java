ca8ba70857132d00cbfe7ebae8d7ac7b7ff0f660
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-03-03 11:14:08 +0700

    'lab3_ver4'

---
+++ app/src/main/java/com/example/androidtuan03/Active3c_screen02.java
@@ -0,0 +1,14 @@
+package com.example.androidtuan03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Active3c_screen02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active3c_screen02);
+    }
+}
